49c48627d4736000e5ddbb46969e7e2935dddc2d
Guybrush Threepwood <giovanni.cappelletti.1973@gmail.com>
2023-05-08 17:31:14 +0200

    correzione live

---
+++ src/org/lessons/java/shop/Main.java
@@ -0,0 +1,10 @@
+package org.lessons.java.shop;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Prodotto p1 = new Prodotto("prod 1", "desc prod 1", 100, 22);
+		System.out.println(p1);
+	}
+}
